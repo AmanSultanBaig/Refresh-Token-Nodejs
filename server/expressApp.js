ffaa38d89e94eba09827f9f261bef46d77e45c29
Aman Sultan Baig <amansultanbaig@gmail.com>
2023-01-20 00:30:07 +0500

    express app created

--- server/expressApp.js
+++ server/expressApp.js
@@ -1,6 +1,13 @@
+const cors = require("cors");
+const express = require("express")
 require("dotenv").config()
+
 module.exports = (app) => {
-    app.get("/", (req, res) => res.send("hello world"))
+    app.get("/", (req, res) => res.send("Express Application!"))
+
+    app.use(cors())
+    app.use(express.json())
+    app.use(express.urlencoded({ extended: true }))
 
     let port = process.env.PORT || 8000;
     app.listen(port, () => console.log(`App is running on port ${port}`))
