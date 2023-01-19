6283c79e75c9a5f3486253b2d1efa7564ea9048a
Aman Sultan Baig <amansultanbaig@gmail.com>
2023-01-20 00:52:37 +0500

    schema designed for user collection

---
+++ server/db/models/user.js
@@ -0,0 +1,31 @@
+const { Schema, model } = require("mongoose");
+
+const UserSchema = Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true
+    },
+}, {
+    timestamps: true,
+    toJSON: {
+        transform: function (doc, ret) {
+            ret.id = ret._id;
+            delete ret._id;
+            delete ret._v;
+        }
+    }
+})
+
+module.exports = model("user", UserSchema)
